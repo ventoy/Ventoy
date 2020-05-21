90fa7adc84f6aa59befeddf5ecdb56cd4a06956c
longpanda <59477474+ventoy@users.noreply.github.com>
2020-05-21 12:46:10 +0800

    Update build_libfuse.sh

--- FUSEISO/build_libfuse.sh
+++ FUSEISO/build_libfuse.sh
@@ -14,16 +14,21 @@ LIBFUSE_DIR=$CUR/LIBFUSE
 rm -rf libfuse
 rm -rf $LIBFUSE_DIR
 
-# please download https://gitee.com/mirrors/libfuse/repository/archive/fuse-2.9.9.zip
-if ! [ -e ../ExFAT/mirrors-libfuse-fuse-2.9.9.zip ]; then
+# please download https://codeload.github.com/libfuse/libfuse/zip/fuse-2.9.9
+if [ -e ../ExFAT/mirrors-libfuse-fuse-2.9.9.zip ]; then
+    rm -rf libfuse
+    unzip ../ExFAT/mirrors-libfuse-fuse-2.9.9.zip
+    cd libfuse
+elif [ -e ../ExFAT/libfuse-fuse-2.9.9.zip ]; then
+    rm -rf libfuse-fuse-2.9.9
+    unzip ../ExFAT/libfuse-fuse-2.9.9.zip
+    cd libfuse-fuse-2.9.9
+else
     echo "Please download mirrors-libfuse-fuse-2.9.9.zip first"
     exit 1
 fi
 
-unzip ../ExFAT/mirrors-libfuse-fuse-2.9.9.zip
 
-
-cd libfuse
 ./makeconf.sh
 
 ./configure --prefix="$LIBFUSE_DIR"
