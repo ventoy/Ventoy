1e75385146c070845e1c23806cab76948564ad15
longpanda <admin@ventoy.net>
2021-01-22 15:33:06 +0800

    update build script

--- INSTALL/all_in_one.sh
+++ INSTALL/all_in_one.sh
@@ -26,37 +26,31 @@ sh build.sh || exit 1
 cd $VTOY_PATH/vtoygpt
 sh build.sh || exit 1
 
-cd $VTOY_PATH/ExFAT
-sh buidlibfuse.sh || exit 1
-sh buidexfat.sh || exit 1
-/bin/cp -a EXFAT/shared/mkexfatfs   $VTOY_PATH/INSTALL/tool/mkexfatfs_64
-/bin/cp -a EXFAT/shared/mount.exfat-fuse   $VTOY_PATH/INSTALL/tool/mount.exfat-fuse_64
-
-
 cd $VTOY_PATH/FUSEISO
 sh build_libfuse.sh
 sh build.sh
 
-cd $VTOY_PATH/SQUASHFS/SRC
-sh build_lz4.sh
-sh build_lzma.sh
-sh build_lzo.sh
-sh build_zstd.sh
 
-cd $VTOY_PATH/SQUASHFS/squashfs-tools-4.4/squashfs-tools
-sh build.sh
+# We almost rarely modifiy these code, so no need to build them everytime
 
-cd $VTOY_PATH/VBLADE/vblade-master
-sh build.sh
-
-cd $VTOY_PATH/Ventoy2Disk/Ventoy2Disk/xz-embedded-20130513/userspace
-make -f ventoy_makefile
-strip --strip-all xzminidec
-rm -f $VTOY_PATH/IMG/cpio/ventoy/tool/xzminidec
-cp -a xzminidec $VTOY_PATH/IMG/cpio/ventoy/tool/xzminidec
-make clean; rm -f *.o
+# cd $VTOY_PATH/ExFAT
+# sh buidlibfuse.sh || exit 1
+# sh buidexfat.sh || exit 1
+# /bin/cp -a EXFAT/shared/mkexfatfs   $VTOY_PATH/INSTALL/tool/mkexfatfs_64
+# /bin/cp -a EXFAT/shared/mount.exfat-fuse   $VTOY_PATH/INSTALL/tool/mount.exfat-fuse_64
 
 
+# cd $VTOY_PATH/SQUASHFS/SRC
+# sh build_lz4.sh
+# sh build_lzma.sh
+# sh build_lzo.sh
+# sh build_zstd.sh
+
+# cd $VTOY_PATH/SQUASHFS/squashfs-tools-4.4/squashfs-tools
+# sh build.sh
+
+# cd $VTOY_PATH/VBLADE/vblade-master
+# sh build.sh
 
 cd $VTOY_PATH/INSTALL
 sh ventoy_pack.sh || exit 1
