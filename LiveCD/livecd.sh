66d5946bd10588a3caa5f903960b02dbd2ab45a4
longpanda <admin@ventoy.net>
2021-02-02 23:24:19 +0800

    VentoyLiveCD x86_64

--- LiveCD/livecd.sh
+++ LiveCD/livecd.sh
@@ -21,9 +21,9 @@ if ! [ -e $VENTOY_PATH/INSTALL/ventoy-${version}-linux.tar.gz ]; then
 fi
 
 rm -rf ISO_TMP
-cp $OPT ISO ISO_TMP
+cp -a ISO ISO_TMP
 
-cp $OPT VTOY VTOY_TMP 
+cp -a VTOY VTOY_TMP 
 
 ls -la
 if ! [ -d ISO_TMP ]; then
@@ -42,13 +42,15 @@ cd VTOY_TMP
 gcc -O2 ./ventoy/disksize.c -o ./ventoy/disksize
 
 rm -f ./ventoy/disksize.c
+
+chmod +x ./ventoy/*.sh
 find . | cpio  -o -H newc | gzip -c -9 > ../ISO_TMP/EFI/ventoy/ventoy.gz
 cd .. && rm -rf VTOY_TMP
 
 
-cp $OPT $VENTOY_PATH/INSTALL/ventoy-${version}-linux.tar.gz ISO_TMP/EFI/ventoy/
-cp $OPT GRUB/cdrom.img ISO_TMP/EFI/boot/
-cp $OPT GRUB/bootx64.efi ISO_TMP/EFI/boot/
+cp -a $VENTOY_PATH/INSTALL/ventoy-${version}-linux.tar.gz ISO_TMP/EFI/ventoy/
+cp -a GRUB/cdrom.img ISO_TMP/EFI/boot/
+cp -a GRUB/bootx64.efi ISO_TMP/EFI/boot/
 
 
 rm -rf efimnt
@@ -63,7 +65,7 @@ cp $OPT GRUB/bootx64.efi efimnt/EFI/boot/
 umount efimnt
 
 sync
-cp $OPT efi.img ISO_TMP/EFI/boot/
+cp -a efi.img ISO_TMP/EFI/boot/
 
 rm -rf efimnt
 rm -f efi.img
