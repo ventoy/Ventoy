be0e19fc5b2422c284e0e1f680a40338c8f9f3ed
longpanda <admin@ventoy.net>
2022-02-06 12:26:36 +0800

    update tip message for recalbox img file.

--- IMG/cpio/ventoy/loop/recalbox/ventoy-share.sh
+++ IMG/cpio/ventoy/loop/recalbox/ventoy-share.sh
@@ -1,15 +1,21 @@
-echo '#################################################' > /dev/console
-echo '#################################################' > /dev/console
-echo '### PLEASE REBOOT AND BOOT FROM VENTOY AGAIN ###'  > /dev/console
-echo '#################################################' > /dev/console
-echo '#################################################' > /dev/console
-
-echo '#################################################' > /dev/tty0
-echo '#################################################' > /dev/tty0
-echo '### PLEASE REBOOT AND BOOT FROM VENTOY AGAIN ###'  > /dev/tty0
-echo '#################################################' > /dev/tty0
-echo '#################################################' > /dev/tty0
-
-
 sync
-sleep 3600
+
+while [ -n "Y" ]; do
+    clear > /dev/console
+    echo '################################################' > /dev/console
+    echo '################################################' > /dev/console
+    echo '### Please reboot and load from Ventoy again ###' > /dev/console
+    echo '### This only needed for the first boot time ###' > /dev/console
+    echo '################################################' > /dev/console
+    echo '################################################' > /dev/console
+
+    clear > /dev/tty0
+    echo '################################################' > /dev/tty0
+    echo '################################################' > /dev/tty0
+    echo '### Please reboot and load from Ventoy again ###' > /dev/tty0
+    echo '### This only needed for the first boot time ###' > /dev/tty0
+    echo '################################################' > /dev/tty0
+    echo '################################################' > /dev/tty0
+    sleep 1
+done
+
