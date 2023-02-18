5a433f49f70da581722ce90a5fb33bac6988ebe8
longpanda <admin@ventoy.net>
2023-02-18 21:33:05 +0800

    Optimization for booting Fedora series.

--- IMG/cpio/ventoy/hook/rhel7/ventoy-inotifyd-call.sh
+++ IMG/cpio/ventoy/hook/rhel7/ventoy-inotifyd-call.sh
@@ -1,6 +1,6 @@
 #!/ventoy/busybox/sh
 #************************************************************************************
-# Copyright (c) 2020, longpanda <admin@ventoy.net>
+# Copyright (c) 2023, longpanda <admin@ventoy.net>
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -22,13 +22,5 @@
 vtHook=$($CAT $VTOY_PATH/inotifyd-hook-script.txt)
 
 vtdisk=$(get_ventoy_disk_name)
-if [ "$vtdisk" = "unknown" ]; then
-    vtlog "... start inotifyd listen $vtHook ..."
-    $BUSYBOX_PATH/nohup $VTOY_PATH/tool/inotifyd $vtHook  /dev:n  2>&-  & 
-else
-    vtlog "... $vtdisk already exist ..."
-    
-    #don't call it too early issue 2225
-    #$BUSYBOX_PATH/sh $vtHook n /dev "${vtdisk#/dev/}2"
-    cp -a  $VTOY_PATH/hook/rhel7/ventoy-inotifyd-call.sh /lib/dracut/hooks/initqueue/settled/90-ventoy-inotifyd-call.sh   
-fi
+vtlog "... $vtdisk already exist ..."
+$BUSYBOX_PATH/sh $vtHook n /dev "${vtdisk#/dev/}2"    
